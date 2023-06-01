899ee149e61e9725710d70b5b34c672e511651e2
richard.liao <richard.liao@tpisoftware.com>
2023-06-01 11:07:32 +0800

    feat: 加入 BottomNavigationBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:let_api_flutter/src/pages/home.dart';
+import 'package:let_api_flutter/src/widgets/bottomNavigationBar.dart';
 
 class App extends StatelessWidget {
   //渲染到畫面
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        //global colors
-        primaryColor: Colors.lightBlue[400],
-        useMaterial3: true,
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
-      ),
-      home: const MyHomePage(title: '首頁'),
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          //global colors
+          primaryColor: Colors.lightBlue[400],
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
+        ),
+        home: const BottomNavigationBarCustom());
   }
 }
 
---
+++ lib/src/widgets/bottomNavigationBar.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+import 'package:let_api_flutter/src/pages/home.dart';
+
+class BottomNavigationBarCustom extends StatefulWidget {
+  const BottomNavigationBarCustom({super.key});
+
+  @override
+  State<BottomNavigationBarCustom> createState() =>
+      _BottomNavigationBarCustomState();
+}
+
+// BottomNavigationBar 狀態管理
+class _BottomNavigationBarCustomState extends State<BottomNavigationBarCustom> {
+  int _selectedIndex = 0;
+
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          _buildOffstageNavigator(0),
+          _buildOffstageNavigator(1),
+          _buildOffstageNavigator(2),
+          _buildOffstageNavigator(3),
+        ],
+      ),
+      bottomNavigationBar:
+          createNavigationBar(context, _selectedIndex, _onItemTapped),
+    );
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  Map<String, WidgetBuilder> _routeBuilders(BuildContext context, int index) {
+    return {
+      '/': (context) {
+        return [
+          const MyHomePage(title: '123'),
+          Text(
+            'Index 1: Business',
+            style: optionStyle,
+          ),
+          Text(
+            'Index 2: School',
+            style: optionStyle,
+          ),
+          Text(
+            'Index 3: Settings',
+            style: optionStyle,
+          ),
+        ].elementAt(index);
+      },
+    };
+  }
+
+//解決換頁時，BottomNavigator 消失的問題
+  Widget _buildOffstageNavigator(int index) {
+    var routeBuilders = _routeBuilders(context, index);
+
+    return Offstage(
+      offstage: _selectedIndex != index,
+      child: Navigator(
+        onGenerateRoute: (routeSettings) {
+          return MaterialPageRoute(
+            builder: (context) => routeBuilders[routeSettings.name]!(context),
+          );
+        },
+      ),
+    );
+  }
+}
+
+//建立 NavigationBar
+Container createNavigationBar(
+    BuildContext context, _selectedIndex, _onItemTapped) {
+  return Container(
+      child: ClipRRect(
+          child: BottomNavigationBar(
+    type: BottomNavigationBarType.fixed,
+    items: const <BottomNavigationBarItem>[
+      BottomNavigationBarItem(
+        icon: Icon(Icons.home),
+        label: 'Home',
+      ),
+      BottomNavigationBarItem(
+        icon: Icon(Icons.business),
+        label: 'Business',
+      ),
+      BottomNavigationBarItem(
+        icon: Icon(Icons.school),
+        label: 'School',
+      ),
+      BottomNavigationBarItem(
+        icon: Icon(Icons.settings),
+        label: 'Settings',
+      ),
+    ],
+    currentIndex: _selectedIndex,
+    selectedItemColor: Theme.of(context).primaryColor,
+    unselectedItemColor: Colors.black45,
+    iconSize: 25,
+    unselectedFontSize: 12,
+    showUnselectedLabels: true,
+    backgroundColor: Colors.white,
+    onTap: _onItemTapped,
+  )));
+}
