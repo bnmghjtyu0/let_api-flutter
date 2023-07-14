09ec324c43b45d1c1a03b6d827df6d7d894023fe
richard.liao <richard.liao@tpisoftware.com>
2023-07-14 11:48:12 +0800

    chore: 使用者頁面切版 - 使用 LayoutBuilder 響應式佈局

--- lib/src/constants/dimensions.dart
+++ lib/src/constants/dimensions.dart
@@ -3,75 +3,90 @@ import 'package:flutter/material.dart';
 /// 定義響應式佈局
 /// 如何使用？
 /// Dimensions(context).height(66)
+/// 假設寬度為 150 realSize 等於 844(螢幕高度) / 150
 class Dimensions {
   final BuildContext context;
   Dimensions(this.context);
 
-  Size displaySize(context) {
+  Size displaySize() {
     return MediaQuery.of(context).size;
   }
 
   double height(double size) {
-    double screenHeight = displaySize(context).height;
+    double screenHeight = displaySize().height;
     double realSize = 844 / size;
     return screenHeight / realSize;
   }
 
   double width(int size) {
-    double screenWidth = displaySize(context).width;
+    double screenWidth = displaySize().width;
     double realSize = 844 / size;
     return screenWidth / realSize;
   }
 
   double radius(double size) {
-    double screenHeight = displaySize(context).height;
+    double screenHeight = displaySize().height;
     double realSize = 844 / size;
     return screenHeight / realSize;
   }
 
   double iconSize(int size) {
-    double screenHeight = displaySize(context).height;
+    double screenHeight = displaySize().height;
     double realSize = 844 / size;
     return screenHeight / realSize;
   }
 
   double bottomHeightBar() {
-    double screenHeight = displaySize(context).height;
+    double screenHeight = displaySize().height;
     return screenHeight / 7.03;
   }
 
   double popularFoodImgSize() {
-    double screenHeight = displaySize(context).height;
+    double screenHeight = displaySize().height;
     return screenHeight / 2.41;
   }
 
   double pageView() {
-    double screenHeight = displaySize(context).height;
+    double screenHeight = displaySize().height;
     return screenHeight / 2.64;
   }
 
   double pageViewContainer() {
-    double screenHeight = displaySize(context).height;
+    double screenHeight = displaySize().height;
     return screenHeight / 3.84;
   }
 
   double pageViewTextContainer() {
-    double screenHeight = displaySize(context).height;
+    double screenHeight = displaySize().height;
     return screenHeight / 7.03;
   }
 
   double listViewImageSize() {
-    double screenWidth = displaySize(context).width;
+    double screenWidth = displaySize().width;
     return screenWidth / 3.25;
   }
 
   double listViewTextContentSize() {
-    double screenWidth = displaySize(context).width;
+    double screenWidth = displaySize().width;
     return screenWidth / 3.9;
   }
 
   double splashImg() {
-    double screenHeight = displaySize(context).height;
+    double screenHeight = displaySize().height;
     return screenHeight / 3.37;
+  }
+
+  ///account avatar 手機
+  double avatarXs() {
+    double screenHeight = displaySize().height;
+    double realSize = 844 / 200;
+    return screenHeight / realSize;
+  }
+
+  ///account avatar 平板
+  double avatarSm() {
+    double screenHeight = displaySize().width;
+    double realSize = 844 / 100;
+    return screenHeight / realSize;
   }
 }
---
+++ lib/src/ui/screens/account/account_tablet_screen.dart
@@ -0,0 +1,75 @@
+import 'package:let_api_flutter/common_libs.dart';
+import 'package:let_api_flutter/src/constants/constants.dart';
+import 'package:let_api_flutter/src/ui/common/widgets/app_header.dart';
+import 'package:let_api_flutter/src/ui/common/widgets/widgets.dart';
+
+class AccountTabletScreen extends StatelessWidget {
+  AccountTabletScreen({Key? key, required this.users}) : super(key: key);
+
+  List<dynamic> users;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        color: $styles.colors.gray01,
+        child: Column(
+          children: [
+            AppHeader(
+              title: 'Profile',
+              backgroundColor: $styles.colors.mainColor,
+            ),
+            Expanded(
+                child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 30),
+                  margin: EdgeInsets.only(left: Dimensions(context).height(60), right: Dimensions(context).height(60)),
+                  width: Dimensions(context).avatarSm(),
+                  height: Dimensions(context).avatarSm(),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(Dimensions(context).avatarSm() / 2),
+                      color: Colors.blue,
+                      image: DecorationImage(fit: BoxFit.cover, image: AssetImage('assets/images/lake.jpg'))),
+                ),
+                Expanded(
+                  child: Wrap(
+                    spacing: 32, //水平
+                    runSpacing: 10, //垂直
+                    children: [
+                      for (var i = 0; i < users.length; i++)
+                        Container(
+                          width: MediaQuery.of(context).size.width / 2 - 100,
+                          margin: EdgeInsets.only(bottom: Dimensions(context).height(20)),
+                          padding: EdgeInsets.symmetric(horizontal: Dimensions(context).height(20)),
+                          decoration: BoxDecoration(color: Colors.white, boxShadow: [
+                            BoxShadow(
+                              blurRadius: 1,
+                              offset: const Offset(0, 2),
+                              color: Colors.grey.withOpacity(0.2),
+                              spreadRadius: 1,
+                            )
+                          ]),
+                          child: Row(children: [
+                            AppIcon(
+                              icon: users[i]['icon'],
+                              iconSize: Dimensions(context).height(20),
+                              color: Colors.white,
+                              backgroundColor: users[i]['iconBackground'],
+                            ),
+                            Container(
+                              margin: EdgeInsets.only(left: Dimensions(context).width(20)),
+                              padding: EdgeInsets.symmetric(vertical: Dimensions(context).height(20)),
+                              child: Text(users[i]['name']),
+                            )
+                          ]),
+                        )
+                    ],
+                  ),
+                )
+              ],
+            )),
+          ],
+        ));
+  }
+}
